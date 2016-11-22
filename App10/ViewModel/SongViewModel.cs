3f98c44b5d11cfa6175d0e8b03cab5215e757b5f
Daniel <Daniel@DK_PC>
2016-11-22 14:53:51 +0100

    property

--- App10/ViewModel/SongViewModel.cs
+++ App10/ViewModel/SongViewModel.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 using App10.Model;
 
 namespace App10.ModelView
 {
-    class SongViewModel
-    {
 
+    class SongViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+
+
+        }
         public AddSongCommand AddSongCommand { get; set;}
-        //test
 
     }
 }
