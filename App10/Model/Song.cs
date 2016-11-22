4273b6fe47929b917a528626d0e17f50c13675ff
Daniel <Daniel@DK_PC>
2016-11-22 14:38:32 +0100

    Add project files.

---
+++ App10/Model/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App10.Model
+{
+    public class Song
+    {
+        public string songName { get; set; }//Navn på sangen
+        public string artistName { get; set; }// kunstner navn
+        public string fromWhere { get; set; } // Fundet sangen i eller fra
+
+        public override string ToString()
+        {
+            return songName + " " + artistName;
+        }
+
+
+    }
+}
